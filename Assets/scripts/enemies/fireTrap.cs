9301cd5fe8a5498f257fb365e75ae61454a2e7a7
Gascuena Leo <gascuenaleo@gmail.com>
2025-03-05 16:47:44 +0100

    UPDATE/ create new enemies/method

---
+++ Assets/scripts/enemies/fireTrap.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+
+public class fireTrap : MonoBehaviour
+{
+    [SerializeField] private float damage;
+    [Header("Firetrap Timers")]
+    [SerializeField] private float activationDelay;
+    [SerializeField] private float activeTime;
+    private Animator anim;
+    private SpriteRenderer spriteRend;
+
+    private bool triggered;
+    private bool active;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (!triggered)
+                StartCoroutine(ActivateFireTrap());
+
+            if (active)
+                collision.GetComponent<health>().TakeDamage(damage);
+        }
+    }
+    private IEnumerator ActivateFireTrap()
+    {
+        triggered = true;
+        spriteRend.color = Color.red; 
+        yield return new WaitForSeconds(activationDelay);
+        spriteRend.color = Color.white;
+        active = true;
+        anim.SetBool("activated", true);
+
+        yield return new WaitForSeconds(activeTime);
+        active = false;
+        triggered = false;
+        anim.SetBool("activated", false);
+
+    }
+}
